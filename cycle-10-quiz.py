21a57a414b8411ce62e20db7626e5db2b87d11b5
p22npiselli <p22npiselli@fairfieldprep.org>
2022-02-02 10:20:14 -0500

    quiz

---
+++ cycle-10-quiz.py
@@ -0,0 +1,45 @@
+# Author: Nolan Piselli
+
+prices = [30, 40, 25, 55, 60, 80, 65]
+# what prices equals
+x = prices[0] + prices[1] + prices[2] + prices[3] + prices[4] + prices[5] + prices[6]
+# print(x)
+# making sure equation works
+
+asp = x / 7
+print(asp)
+# final equation
+
+
+prices = [30, 40, 25, 55, 60, 80, 65]
+# defining prices
+a = prices[0] - 5
+b = prices[1] - 5
+c = prices[2] - 5
+d = prices[3] - 5
+e = prices[4] - 5
+f = prices[5] - 5
+g = prices[6] - 5
+# subtracting evry value by 5
+
+prices_subtracted = [a, b, c, d, e, f, g]
+print(prices_subtracted)
+# final solution
+
+prices = [30, 40, 25, 55, 60, 80, 65]
+total_prices = [30, 40, 25, 45, 50, 70, 55]
+# variables created
+def find_prices(total_prices):
+ # function  
+   total_prices = [30, 40, 25, 45, 50, 70, 55]
+   
+   print(total_prices)
+   
+
+print(find_prices)
+
+prices = [30, 40, 25, 55, 60, 80, 65]
+items = [['tee-shirt','long-sleeved','tanktop'],['quarter-zip','pullover','full-zip',half-zip]]
+
+    
+
